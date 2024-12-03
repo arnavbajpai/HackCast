723726d2fdedd908fea60fd022b8961ff043a7cb
Vangmay Sachan <vangmay.sachan16@gmail.com>
2024-12-04 02:43:21 +0530

    Make better frontend

--- hackernews_voice/src/App.jsx
+++ hackernews_voice/src/App.jsx
@@ -1,11 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Box, Heading, Link, Text } from "@chakra-ui/react";
+
+const BASEURL = "http://localhost:5000";
+
+const fetchAudio = (audioFileId) => {
+  return `${BASEURL}/audio/${audioFileId}`;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASEURL}/posts`).then((posts) => {
+      console.log(posts.data);
+      setPosts(posts.data);
+    });
+  }, []);
 
   return (
     <>
-      <h1>Hello world</h1>
+      <Box p={4}>
+        <Heading as="h1" size="xl" mb={6}>
+          Hackernews-Podcasts
+        </Heading>
+
+        {posts.map((post) => (
+          <Box
+            key={post._id}
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            p={4}
+            mb={6}
+          >
+            <Heading as="h3" size="md" mb={2}>
+              {post.title}
+            </Heading>
+            <Link
+              href={post.url}
+              isExternal
+              color="teal.500"
+              fontWeight="bold"
+              mb={2}
+            >
+              Read more
+            </Link>
+            <Text mt={4} fontSize="sm" color="gray.600">
+              <audio controls>
+                <source
+                  src={fetchAudio(post.audio_file_id)}
+                  type="audio/mpeg"
+                />
+                Your browser does not support the audio element.
+              </audio>
+            </Text>
+          </Box>
+        ))}
+      </Box>
     </>
   );
 }
