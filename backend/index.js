b2d653befa06c12fdfbb2c17c8a953a92d6181ba
Vangmay Sachan <vangmay.sachan16@gmail.com>
2024-12-08 16:19:51 +0530

    STILL TRYING TO FIGURE OUT WEBHOOKS

--- backend/index.js
+++ backend/index.js
@@ -81,6 +81,9 @@ app.post("/webhook", async (req, res) => {
   return <p>{req.body}</p>;
 });
 
+app.get("/webhook", async (req, res) => {
+  res.json(<h1>ALL HAIL THE WEBHOOK</h1>);
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
